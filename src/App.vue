be315afc2208c98e4520db91bb1798331393acc1
Kanade <874996596@qq.com>
2020-03-31 20:09:56 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,113 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<v-app id="inspire">
+		<v-navigation-drawer v-model="drawer" app>
+			<v-list>
+				<v-subheader>REPORTS</v-subheader>
+				<v-list-item-group v-model="item" color="primary">
+					<v-list-item v-for="(item, i) in items" :key="i" :href="item.url">
+						<v-list-item-icon>
+							<v-icon v-text="item.icon"></v-icon>
+						</v-list-item-icon>
+					
+							<v-list-item-content>
+								<v-list-item-title v-text="item.text"></v-list-item-title>
+							</v-list-item-content>
+			
+					</v-list-item>
+				</v-list-item-group>
+			</v-list>
+			<!-- <v-list dense>
+				<v-list-item link>
+					<v-list-item-action>
+						<v-icon>mdi-home</v-icon>
+					</v-list-item-action>
+						<v-list-item-content>
+							<router-link to="/">
+								<v-list-item-title>
+										home
+								</v-list-item-title>
+							</router-link>
+						</v-list-item-content>
+				</v-list-item>
+				<v-list-item link>
+					<v-list-item-action>
+						<v-icon>mdi-home</v-icon>
+					</v-list-item-action>
+					
+					<v-list-item-content>
+						<router-link to="/About">
+							<v-list-item-title>
+								
+									About
+								
+							</v-list-item-title>
+						</router-link>
+					</v-list-item-content>
+				</v-list-item>
+			</v-list> -->
+		</v-navigation-drawer>
+
+
+		<v-app-bar app color="indigo" dark>
+			<v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+			<v-toolbar-title>Application</v-toolbar-title>
+		</v-app-bar>
+
+		<v-content>
+			<v-container class="fill-height" fluid>
+				<router-view></router-view>
+
+			</v-container>
+		</v-content>
+		<v-footer color="indigo" app>
+			<span class="white--text">&copy; 2019</span>
+		</v-footer>
+	</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	export default({
+		data: () => ({
+			item: 1,
+			drawer:true,
+			items: [{
+					text: 'Real-Time',
+					icon: 'mdi-clock',
+					url:"#/	"
+				},
+				{
+					text: 'Audience',
+					icon: 'mdi-account',
+					url:"#/home"
+				},
+				{
+					text: 'Conversions',
+					icon: 'mdi-flag',
+					url:"#/about"
+				},
+			],
+		}),
+	})
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+	}
+
+	#nav {
+		padding: 30px;
+	}
+
+	#nav a {
+		font-weight: bold;
+		color: #2c3e50;
+	}
+
+	#nav a.router-link-exact-active {
+		color: #42b983;
+	}
 </style>
